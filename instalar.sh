9d28207dade3051048ab964d431cd96c0f38cfe0
Tiago Pereira <daragao689@gmail.com>
2020-04-21 09:09:38 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -12,7 +12,7 @@ sudo apt update && sudo apt dist-upgrade -y &&
 
 sudo apt install ttf-mscorefonts-installer software-properties-common apt-transport-https curl python3 python-pip git build-essential libssl-dev flatpak gnome-software-plugin-flatpak openjdk-14-jre-headless openjdk-14-jdk-headless icedtea-netx -y &&
 
-## Instalando browsers ##
+## Instalando chrome e brave ##
 
 cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb &&
 cd ~/Downloads/ && curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - &&
